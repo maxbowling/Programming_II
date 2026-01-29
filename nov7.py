14046227028eaf19688398c4b61685f92a2d2964
Max <max@gmail.com>
2026-01-29 13:26:31 -0600

    Changed plot color to green

--- nov7.py
+++ nov7.py
@@ -4,7 +4,7 @@ fig,ax = plt.subplots()
 fig.set_facecolor('lightblue')
 fig.suptitle('Cool Graph')
 ax.grid()
-graph = ax.scatter([1,2,3,4,5,6,7,8,9,10],[1,2,3,4,2,7,8,5,9,10,],color='red')
+graph = ax.scatter([1,2,3,4,5,6,7,8,9,10],[1,2,3,4,2,7,8,5,9,10,],color='green')
 
 plt.savefig("Example4.png")
 plt.show()
