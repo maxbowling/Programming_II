3c3c37514cbd3125df714bbbf205dddeef3fbfca
Max <max@gmail.com>
2026-02-02 22:38:29 -0600

    Added print statement saying I am just having fun on this file until I start the project

--- springmidterm.py
+++ springmidterm.py
@@ -8,3 +8,4 @@ graph = ax.scatter([1,2,3,4,5,6,7,8,9,10],[1,2,3,4,2,7,8,5,9,10,],color='green')
 
 plt.savefig("Example4.png")
 plt.show()
+print("I am just messing around on this file until I start my project")
