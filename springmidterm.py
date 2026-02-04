72e80de5e65426b27bd9cdd4db33722f242ef653
Max <max@gmail.com>
2026-02-04 09:00:39 -0600

    Changed name to blue graph

--- springmidterm.py
+++ springmidterm.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 fig,ax = plt.subplots()
 fig.set_facecolor('lightblue')
-fig.suptitle('Cool Graph')
+fig.suptitle('Blue Graph')
 ax.grid()
 graph = ax.scatter([1,2,3,4,5,6,7,8,9,10],[1,2,3,4,2,7,8,5,9,10,],color='blue')
 
